bc15bfa7defbb897986700a66a6f7d63f8a98e55
DucPPhan <phanminhduc23@gmail.com>
2025-05-29 15:03:30 +0700

    set up environment

--- pharmacos/src/App.tsx
+++ pharmacos/src/App.tsx
@@ -1,9 +1,31 @@
+import { Suspense, lazy } from "react";
+import { useRoutes, Routes, Route } from "react-router-dom";
+// import Home from "./components/home";
+import routes from "tempo-routes";
+// import Login from "./pages/login";
+// import Cart from "./pages/cart";
+// import Profile from "./pages/profile";
+// import PersonalInfo from "./pages/profile/personal-info";
+// import ChangePassword from "./pages/profile/change-password";
+// import AdminDashboard from "./pages/admin/dashboard";
+
 function App() {
   return (
-    <>
-      <div className="font-bold">check</div>
-    </>
-  )
+    <Suspense fallback={<p>Loading...</p>}>
+      <>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/personal-info" element={<PersonalInfo />} />
+          <Route path="/profile/change-password" element={<ChangePassword />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+        </Routes>
+        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+      </>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
