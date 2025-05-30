0e4ff700ec4728199e037fda98d487327deb6d46
DucPPhan <phanminhduc23@gmail.com>
2025-05-30 12:55:18 +0700

    commit home

--- pharmacos/src/App.tsx
+++ pharmacos/src/App.tsx
@@ -1,34 +1,24 @@
 import { Suspense, lazy } from "react";
-import { useRoutes, Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "./screens/Home";
-// import routes from "tempo-routes";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-// import Login from "./pages/login";
-// import Cart from "./pages/cart";
-// import Profile from "./pages/profile";
-// import PersonalInfo from "./pages/profile/personal-info";
-// import ChangePassword from "./pages/profile/change-password";
-// import AdminDashboard from "./pages/admin/dashboard";
+import { useRoutes, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import routes from "tempo-routes";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Suspense fallback={<p>Loading...</p>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/login" element={<Login />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/profile/personal-info" element={<PersonalInfo />} />
-            <Route path="/profile/change-password" element={<ChangePassword />} />
-            <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
-          </Routes>
-          {/* {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)} */}
-      </Suspense>
-      <Footer />
-    </Router>
+    <Suspense fallback={<p>Loading...</p>}>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/personal-info" element={<PersonalInfo />} />
+          <Route path="/profile/change-password" element={<ChangePassword />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+        </Routes>
+        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+      </>
+    </Suspense>
   );
 }
 
