d5dd73bcdce4d73b8c2d1334c07a872a209fcd66
minhlahunday <minhlahunday3@gmail.com>
2025-05-30 15:28:42 +0700

    admindashboard
    hihi

--- pharmacos/src/App.tsx
+++ pharmacos/src/App.tsx
@@ -1,9 +1,38 @@
+<<<<<<< Updated upstream
 function App() {
   return (
     <>
       <div className="font-bold">check</div>
     </>
   )
+=======
+import { Suspense, lazy } from "react";
+import { useRoutes, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import routes from "tempo-routes";
+import Dashboard from "./page/admin/Admindashboard";
+
+function App() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/personal-info" element={<PersonalInfo />} />
+          <Route path="/profile/change-password" element={<ChangePassword />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+          <Route path="/admin/dashboard" element={<Dashboard/>} 
+          
+          />
+        </Routes>
+        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+      </>
+    </Suspense>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
