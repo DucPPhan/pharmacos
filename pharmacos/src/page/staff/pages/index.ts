61028bb732313e3159f9fec2e3d2a4a229739d8c
minhlahunday <minhlahunday3@gmail.com>
2025-07-06 22:27:50 +0700

    Create index.ts

---
+++ pharmacos/src/page/staff/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Analytics } from './Analytics';
+export { default as AnalyticsCard } from './AnalyticsCard';
+export { default as SalesChart } from './SalesChart';
+export { default as ProductsTable } from './ProductsTable';
+export { default as InventoryGrid } from './InventoryGrid'; 
